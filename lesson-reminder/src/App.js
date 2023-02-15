6b2ba7a4f088f1a969de3f50a01d0d3f8f501e10
nurantezer <nurantezer68@gmail.com>
2023-02-15 16:49:28 +0300

    end

--- lesson-reminder/src/App.js
+++ lesson-reminder/src/App.js
@@ -1,10 +1,14 @@
-
+import Reminder from "./Reminder";
+import {data} from "./data"
 
 
 function App() {
   return (
     <div >
-      
+      {data.map(({id, name, hour, image }) =>(
+      <Reminder key={id} name={name} hour={hour} img={image}/>  
+      ))}
+    
     </div>
   );
 }
