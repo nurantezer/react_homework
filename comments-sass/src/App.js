1135495a0bcb37187ba0dbc48209c45dbcb00aaa
nurantezer <nurantezer68@gmail.com>
2023-02-16 13:47:46 +0300

    sass

--- comments-sass/src/App.js
+++ comments-sass/src/App.js
@@ -1,8 +1,14 @@
-
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import data from "./data"
+import "./scss/app.scss"
 function App() {
   return (
     <div >
-      
+      <Header/>
+      <Card data={data}/>
+      <Footer/>
     </div>
   );
 }
