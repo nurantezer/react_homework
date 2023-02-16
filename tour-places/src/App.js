15af1e1b98c926d5f625a30648beb29daed477d4
nurantezer <nurantezer68@gmail.com>
2023-02-16 18:38:51 +0300

    update

--- tour-places/src/App.js
+++ tour-places/src/App.js
@@ -1,9 +1,14 @@
-
+import './App.scss'
+import Navbar from './components/Navbar/Navbar'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 
 function App() {
   return (
-    <div >
-      
+    <div className = "App">
+      <Navbar/>
+      <Header/>
+      <Main/>
     </div>
   );
 }
