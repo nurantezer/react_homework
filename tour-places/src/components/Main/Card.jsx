e5480cb7fc98faa343bbe68ea3e003e5bd0b21c5
nurantezer <nurantezer68@gmail.com>
2023-02-16 19:11:26 +0300

    end

--- tour-places/src/components/Main/Card.jsx
+++ tour-places/src/components/Main/Card.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const CArd = () => {
+const Card = (data) => {
+  const{id,title,image,desc} = data;
   return (
-    <div>CArd</div>
+    <div className='cards'>
+      <div className='title'>
+    <h1>{title}</h1>
+      </div>
+    <img src={image} alt="" />
+
+    <div className='card-over'>
+    <p>
+      {desc}
+    </p>
+    </div>
+    </div>
   )
 }
 
-export default CArd
+export default Card;
