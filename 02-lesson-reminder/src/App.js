25b5f279dce6a190bd1cb48718310ddd5a23de0d
nurantezer <nurantezer68@gmail.com>
2023-02-22 12:48:43 +0300

    start

--- 02-lesson-reminder/src/App.js
+++ 02-lesson-reminder/src/App.js
@@ -1,20 +1,10 @@
-import Lesson from "./pages/Lesson";
-import { data } from "./helper/data";
-import Header from "./components/header/Header";
 
-console.log(data);
+
+console.log();
 function App() {
   return (
     <div>
-      <Header />
-      <div className="lesson">
-        <div className="bas">Lesson Reminder</div>
-        <div className="lesson-card">
-          {data.map((item, index) => {
-            return <Lesson key={index} item={item} />;
-          })}
-        </div>
-      </div>
+      
     </div>
   );
 }
