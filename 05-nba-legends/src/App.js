687bc9b0e2768171741aa6a6ef0a79d61aed05bc
nurantezer <nurantezer68@gmail.com>
2023-02-22 18:12:16 +0300

    end

--- 05-nba-legends/src/App.js
+++ 05-nba-legends/src/App.js
@@ -3,9 +3,9 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <Container className='text-center mt-4'>
-     <Header/>
-       
+    <Container className="text-center mt-4">
+      <Header />
+      
     </Container>
   );
 }
