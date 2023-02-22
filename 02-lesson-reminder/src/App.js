6ab3a3e301024e0658e3b7ec9faedc61f30b0a9f
nurantezer <nurantezer68@gmail.com>
2023-02-22 12:58:55 +0300

    header

--- 02-lesson-reminder/src/App.js
+++ 02-lesson-reminder/src/App.js
@@ -1,10 +1,11 @@
+import Header from "./components/Header/Header";
 
 
 console.log();
 function App() {
   return (
     <div>
-      
+      <Header/>
     </div>
   );
 }
---
+++ 02-lesson-reminder/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+        <nav className="nav">
+            <img
+          src="https://clarusway.com/wp-content/uploads/2022/02/Adsiz-tasarim-4-300x78.png"
+          alt="logo"
+          className="logo"
+          />
+        </nav>
+        <h1>Lesson Reminder</h1>
+    </header>
+  )
+}
+
+export default Header
