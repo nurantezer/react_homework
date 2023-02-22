3004deae43f393701a6e8df16bad1b682e2942bd
nurantezer <nurantezer68@gmail.com>
2023-02-22 17:48:04 +0300

    go

--- 05-nba-legends/src/App.js
+++ 05-nba-legends/src/App.js
@@ -1,12 +1,9 @@
-import Content from "./components/Content";
-import Header from "./components/Header";
-import {data} from "./data"
+
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Content data={data}/>
+     
        
     </div>
   );
