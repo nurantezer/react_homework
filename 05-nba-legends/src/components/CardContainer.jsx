53bc240c62c98cf0ea54457281b4b87ab47d1a9e
nurantezer <nurantezer68@gmail.com>
2023-02-22 18:02:58 +0300

    go

---
+++ 05-nba-legends/src/components/CardContainer.jsx
@@ -0,0 +1,16 @@
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import { data } from "../helpers/data";
+import PlayerCard from "./PlayerCard";
+import { useState } from "react";
+
+const CardContainer = () => {
+  return (
+    <div>CardContainer</div>
+  )
+}
+
+export default CardContainer
