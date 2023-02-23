684a4bdf8fb89c4474760318f7d00214cd524b2f
nurantezer <nurantezer68@gmail.com>
2023-02-23 17:37:30 +0300

    go

---
+++ 05-nba-legends/src/components/CardContainer.jsx
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import PlayerCard from "./PlayerCard"
+const CardContainer = () => {
+  return (
+    <Container className="card-container rounded-4 my-4 p-3">
+        <Row className="g-3 justfiy-content-center">
+        {data.map((player,index) =>{
+            return(
+                <Col md={6} lg={4} xl={3} key={index} >
+                    <PlayerCard {...player}/>
+                </Col>
+            )
+        })}
+        </Row>
+
+
+    </Container>
+  )
+}
+
+export default CardContainer
