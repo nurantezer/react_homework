9c98de0f90bfaa5280fbb00140bdfd611d074b8d
nurantezer <nurantezer68@gmail.com>
2023-02-26 19:01:19 +0300

    finished project

--- 10-jtours/src/App.js
+++ 10-jtours/src/App.js
@@ -7,25 +7,30 @@ function App() {
   const [tours, setTours] = useState([]);
   const [loading, setLoading] = useState(false);
 
-
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+  //! çok önemli tıklanılan hariç olan verileri alır tekrar tours a set yöntemiyle gönderip render yapar
+  //! böylelikle veriyi silmiş gibi oluruz sadece tıklanmamış veriler kalır
+  //! burda fonksiyonu yazdık proplarla tour componentine gönderdik ve kullandık
   const fetchData = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
       const res = await fetch(url);
       const data = await res.json();
       console.log(data);
       setTours(data);
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
       console.log(error);
-      setLoading(false)
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
-
 
   if (loading) {
     return (
@@ -40,20 +45,19 @@ function App() {
       <main>
         <div className="title">
           <h2>no tours left</h2>
-          <button onClick={() => fetchData()}>
+          <button className="btn" onClick={fetchData}>
             refresh
           </button>
         </div>
       </main>
     );
-
   }
 
   return (
     <main>
-        <Tours tours={tours}/>
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
-  )
+  );
 }
 
 export default App;
--- 10-jtours/src/Tours.jsx
+++ 10-jtours/src/Tours.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Tour from "./Tour";
 
-const Tours = () => {
-  return <div>Tours</div>;
+const Tours = ({tours, removeTour}) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour)=> {
+            return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        }) }
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
