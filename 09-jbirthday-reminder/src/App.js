6ec11dd3bf88e442fda14a1ea4cb87a1e0162a82
nurantezer <nurantezer68@gmail.com>
2023-02-26 17:33:08 +0300

    updated

--- 09-jbirthday-reminder/src/App.js
+++ 09-jbirthday-reminder/src/App.js
@@ -1,10 +1,23 @@
-
+import { useState } from "react";
+import {data} from "./data";
+import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data)
+
   return (
-    <div className="App">
-   sdfsdfsdgfdgdf
-    </div>
+    <main>
+      <section className="container">
+        <h3>{data.length} birthdays today</h3>
+        <List  people={people}/>
+        <button onClick={() => setPeople([])}>
+          clear all
+        </button>
+        <button onClick={() => setPeople(data)}>
+          Nuran
+        </button>
+      </section>
+    </main>
   );
 }
 
