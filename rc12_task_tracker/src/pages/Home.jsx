63b854e1fd2793142f0ced5fea4c1b0690322c38
nurantezer <nurantezer68@gmail.com>
2023-03-04 17:49:36 +0300

    add list

--- rc12_task_tracker/src/pages/Home.jsx
+++ rc12_task_tracker/src/pages/Home.jsx
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react"
 import AddTask from "../components/addTask/AddTask" 
 import TaskList from "../components/taskList/TaskList"
 import axios from "axios"
-
+import Button from "react-bootstrap/Button"
 const Home = () => {
   const [task, setTask] = useState([])
-   const url = "https://63516c99dfe45bbd55bfd9a2.mockapi.io/api/tasks";
+  const [text, setText] = useState("Show Task Bar")
+  const [isOpen, setIsOpen] = useState(false)
+  const url = "https://63516c99dfe45bbd55bfd9a2.mockapi.io/api/tasks";
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    const buttonText = isOpen ? "Show Task Bar" : "Hide Task Bar"
+    setText(buttonText)
+  }
 
   const getTask = async () => {
     const { data } = await axios(url)
@@ -17,11 +24,19 @@ const Home = () => {
   }, [])
   
   return (
-      <div>
-      <AddTask getTask={getTask} />
+    <div className="mt-4 d-flex justify-content-center flex-column">
+      <Button
+        variant="danger"
+        onClick={() => {
+          toggle();
+        }}
+      >
+        {text}
+      </Button>
+      {isOpen && <AddTask getTask={getTask} />}
       <TaskList task={task} getTask={getTask} />
     </div>
-  )
+  );
 }
 
 export default Home
