7aafe41525ef003ddb158071cf171cfe19217751
nurantezer <nurantezer68@gmail.com>
2023-03-04 17:07:01 +0300

    addtask

--- rc12_task_tracker/src/pages/Home.jsx
+++ rc12_task_tracker/src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react"
 import AddTask from "../components/addTask/AddTask" 
+import TaskList from "../components/taskList/TaskList"
+import axios from "axios"
 
 const Home = () => {
+  const [task, setTask] = useState([])
+   const url = "https://63516c99dfe45bbd55bfd9a2.mockapi.io/api/tasks";
+
+  const getTask = async () => {
+    const { data } = await axios(url)
+    setTask(data)
+    console.log(data);
+  }
+  useEffect(() => {
+    getTask()
+  }, [])
+  
   return (
       <div>
-          <AddTask/>
+      <AddTask getTask={getTask} />
+      <TaskList task={task} getTask={getTask} />
     </div>
   )
 }
