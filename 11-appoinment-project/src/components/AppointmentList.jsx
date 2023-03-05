1ed036e82a23ed2640458f3a51bea2891cbb1346
nurantezer <nurantezer68@gmail.com>
2023-03-05 16:55:01 +0300

    doctors

--- 11-appoinment-project/src/components/AppointmentList.jsx
+++ 11-appoinment-project/src/components/AppointmentList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AppointmentList = () => {
   return (
-    <div>AppointmentList</div>
+    <div></div>
   )
 }
 
--- 11-appoinment-project/src/components/Doctors.jsx
+++ 11-appoinment-project/src/components/Doctors.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useState } from "react";
+   
+const Doctors = ({ doctors, appointments, setAppointments }) => {
+  const [show, setShow] = useState(false)
+  const [selectedDrName, setSelectedDrName] = useState("")
 
-const Doctors = () => {
+
+  const handleClick = (drName) => {
+    setShow(true)
+    setSelectedDrName(drName)
+  } 
   return (
-    <div>Doctors</div>
-  )
+    <Container className="p-2">
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
+        Our Doctors
+      </h3>
+
+      <Row className="justify-content-center">
+        {doctors.map((dr) => (
+          <Col key={dr.id} xs={6} sm={4} md={3}>
+            <img src={dr.img} alt={dr.name}
+              className="img-thumbnail doctor-img"
+              onClick={() => handleClick(dr.name)}
+            />
+            <h5>{dr.name}</h5>
+            <h6>{dr.dep}</h6>
+          </Col>
+        ))}
+
+      </Row>
+  
+    </Container>
+  );
 }
 
 export default Doctors
