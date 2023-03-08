209df96d7d954b5dcb075e50b76350441940c60f
nurantezer <nurantezer68@gmail.com>
2023-03-08 15:26:24 +0300

    home scss

--- 14_wednesday_addams/src/pages/home/Home.jsx
+++ 14_wednesday_addams/src/pages/home/Home.jsx
@@ -2,7 +2,15 @@ import "./Home.scss";
 
 const Home = () => {
   return (
-    <div></div>
+    <div className="home-bgImg-container">
+      <main className="home">
+        <h2>Hi My Name Is</h2>
+        <h1 className="home__name">
+          Wednesday <span className="home__name--last">Addams</span>
+        </h1>
+        <h2>Full Stack Web Developer</h2>
+      </main>
+    </div>
   )
 }
 
