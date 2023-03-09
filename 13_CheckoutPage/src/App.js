4de94c474d1754f4a079fbb8cf673a5621589aa4
nurantezer <nurantezer68@gmail.com>
2023-03-09 13:23:53 +0300

    navbar

--- 13_CheckoutPage/src/App.js
+++ 13_CheckoutPage/src/App.js
@@ -1,9 +1,14 @@
-
-
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div>
-      
+    <div className="bg-light app">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
      
     </div>
   );
