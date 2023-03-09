c7951d9addbc5cbb23e8ee062d914ad8396f9f14
nurantezer <nurantezer68@gmail.com>
2023-03-09 15:23:59 +0300

    update

--- 13_CheckoutPage/src/App.js
+++ 13_CheckoutPage/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Navbar from "./components/Navbar";
 import Main from "./pages/Main";
 import NewProduct from "./pages/NewProduct";
+import ProductList from "./pages/ProductList";
 function App() {
   return (
     <div className="bg-light app">
@@ -10,6 +11,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Main/>} />
           <Route path="/new-product" element={<NewProduct/>} />
+          <Route path="/product-list" element={<ProductList />} />
+          
         </Routes>
       </BrowserRouter>
      
--- 13_CheckoutPage/src/components/ProductCard.jsx
+++ 13_CheckoutPage/src/components/ProductCard.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const ProductCard = () => {
-  return (
-    <div>ProductCard</div>
-  )
+const ProductCard = ({ item, getProducts }) => {
+  const url = process.env.REACT_APP_API_URL;
+  const { name, image, price, dampingRate, amount, id } = item;
+  const navigate = useNavigate(); //!name tıkladığımızda update-product sayfasına gitmek için useNavigate hookuna ihtiyacımız var
+  
 }
 
 export default ProductCard
