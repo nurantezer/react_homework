5833a55fb522956cfe5af7eca582dd1307e6ea46
nurantezer <nurantezer68@gmail.com>
2023-03-12 22:14:33 +0300

    home

--- 16_clarus-library/src/router/AppRouter.jsx
+++ 16_clarus-library/src/router/AppRouter.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/navbar/Navbar";
 import { GlobalStyles } from "../styles/Global.styles";
-
+import Home from "../pages/home/Home";
 
 const AppRouter = () => {
   const [currentUser, setCurrentUser] = useState(
@@ -12,7 +12,9 @@ const AppRouter = () => {
     <BrowserRouter>
       <Navbar currentUser={currentUser} setCurrentUser={setCurrentUser} />
       <GlobalStyles />
-      
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </BrowserRouter>
   );
 };
