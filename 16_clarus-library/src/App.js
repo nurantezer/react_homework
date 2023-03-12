a1d5cdd011496adb052b983b6b638a311b7db942
nurantezer <nurantezer68@gmail.com>
2023-03-12 19:54:16 +0300

    go

--- 16_clarus-library/src/App.js
+++ 16_clarus-library/src/App.js
@@ -1,22 +1,13 @@
-import AppRouter from './router/AppRouter';
-import ThemeProvider from "styled-components";
-import { useEffect, useState } from 'react';
-import {darktheme, lightheme} from "./styles/theme"
-
+import AppRouter from "./router/AppRouter";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./styles/theme";
+// import { GlobalStyles } from "./styles/Global.styles";
 
 function App() {
-  const [myTheme, setMyTheme] = useState(
-    sessionStorage.getItem("theme") || "light"
-  )//kullanıcı seçimi için. eğer başlangıcta storage da veri varsa onu getir yoksa light olarak initial degerini belirle
-  const [themes, setThemes] = useState(lightheme)//Themeprovidera vereceğimiz theme state.başlangıç için lighttheme yi seçtik
-  useEffect(() => {
-    sessionStorage.setItem("theme", myTheme)//kullanıcı refresh atsa bile theme seçimi değişmeyecek
-    setThemes(myTheme === "light" ? lightheme : darktheme)
-  }, [myTheme])//myTheme değiştikçe storage ve themes güncelenecek. Böylelikle Themeprovidera verdiğimiz theme güncellenmiş olacak. Ve sayfa refresh olduğunda kullanıcının theme tercihi değişmemesi için storage da güncellenmiş oluyor. 
-  
   return (
-    <ThemeProvider theme={themes}>
-      <AppRouter myTheme={myTheme} setMyTheme={setMyTheme} />{/* statelerimizi approuter aracılığıyla footera yollayacağız */}
+    <ThemeProvider theme={theme}>
+      {/* <GlobalStyles /> */}
+      <AppRouter />
     </ThemeProvider>
   );
 }
