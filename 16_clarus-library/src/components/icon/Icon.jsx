3c05de95d242d1aa812f95c9fdd871ebc59212c3
nurantezer <nurantezer68@gmail.com>
2023-03-13 12:15:18 +0300

    about

---
+++ 16_clarus-library/src/components/icon/Icon.jsx
@@ -0,0 +1,18 @@
+import { SvgIcon } from "./Icon.style"
+
+const Icon = ({path, icon, hoverColor}) => {
+  return (
+       <SvgIcon
+      hoverColor={hoverColor}
+      xmlns="http://www.w3.org/2000/svg"
+      width={32}
+      height={32}
+      fill="currentColor"
+      className={`bi bi-${icon}`}
+      >
+      <path d={path} />
+      </SvgIcon>
+  )
+}
+
+export default Icon
