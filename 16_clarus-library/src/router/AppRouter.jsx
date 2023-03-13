47d3058ae731dd0b04637dc06f7d89a986c73b8e
nurantezer <nurantezer68@gmail.com>
2023-03-13 11:26:08 +0300

    register

--- 16_clarus-library/src/router/AppRouter.jsx
+++ 16_clarus-library/src/router/AppRouter.jsx
@@ -5,6 +5,7 @@ import { GlobalStyles } from "../styles/Global.styles";
 import Home from "../pages/home/Home";
 import Detail from "../pages/detail/Detail";
 import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 
 const AppRouter = () => {
   const [currentUser, setCurrentUser] = useState(
@@ -17,7 +18,9 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login setCurrentUser={setCurrentUser} />} />
-        <Route path="/detail/:id" element={<Detail/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/detail/:id" element={<Detail />} />
+        
       </Routes>
     </BrowserRouter>
   );
