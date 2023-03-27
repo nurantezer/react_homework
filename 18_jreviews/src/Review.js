cad16766aed38ae10d0968c69dd95962c70fce40
nurantezer <nurantezer68@gmail.com>
2023-03-27 14:04:38 +0300

    if

--- 18_jreviews/src/Review.js
+++ 18_jreviews/src/Review.js
@@ -4,7 +4,16 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
   const [index, setIndex] = useState(0)
-  const {name, job, image, text} = people[index]
+  const { name, job, image, text } = people[index]
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number
+  }
   return (
     <article className='review'>
       <div className="img-container">
