ea7a7f2b636cdc707da2fdfe9e07c11b82139554
nurantezer <nurantezer68@gmail.com>
2023-04-03 13:14:26 +0300

    css

--- 21_jtabs/src/App.js
+++ 21_jtabs/src/App.js
@@ -24,7 +24,7 @@ function App() {
       </section>
     )
   }
-  const {company, dates, duties, title} = jobs[value]
+  const { company, dates, duties, title } = jobs[value]
   return (
     <section className="section">
       <div className="title">
@@ -32,32 +32,36 @@ function App() {
         <div className="underline"></div>
       </div>
       <div className="jobs-center">
-        {jobs.map((item, index) => {
-          return (
-            <button
-              key={item.id}
-              onClick={() => setValue(index)}
-              className={`job-btn ${index === value && 'active-btn'}`}
-            >
-              {item.company}
-            </button>
-          )
-        })}
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
       </div>
-      <article className="job-info">
-        <h3>{title}</h3>
-        <h4>{company}</h4>
-        <p className="job-date">{dates}</p>
-        {duties.map((duty, index) => {
-          return (
-            <div key={index} className="job-desc">
-              <FaAngleDoubleRight className='job-icon'></FaAngleDoubleRight>
-              <p>{duty}</p>
-            </div>
-          )
-        })}
-      </article>
-      <button type='button' className="btn">
+      <button type="button" className="btn">
         more info
       </button>
     </section>
