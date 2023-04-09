d4792bbfcdb8eadae72e66ef3ffa9de747dd9e21
nurantezer <nurantezer68@gmail.com>
2023-04-09 11:37:13 +0300

    if else if

--- 25_grocery-bud/src/App.js
+++ 25_grocery-bud/src/App.js
@@ -1,9 +1,52 @@
 import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem('list')))
+  } else {
+    return []
+  }
+}
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState(getLocalStorage())
+  const [isEditing, setIsEditing] = useState(false)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!name) {
+      showAlert(true, 'danger', 'please enter value')
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return {...item, title: name}
+          }
+          return item
+        })
+      )
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true, 'success', 'value changed')
+    } else {
+      showAlert(true, 'success', 'item added to list')
+      const newItem = { id: new Date().getItem().toString(), title: name }
+      
+      setList([...list, newItem])
+      setName('')
+    }
+
+  }
+  
+  return (
+    <section className="section-center">
+      <form onClick={handleSubmit} className="grocery-from"></form>
+    </section>
+  )
 }
 
 export default App
