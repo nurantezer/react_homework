e4dc601399d29d66abee3de7a022a76ce5019d9e
nurantezer <nurantezer68@gmail.com>
2023-04-11 06:44:40 +0300

    modal css

--- 27_jsidebar-modal/src/Modal.js
+++ 27_jsidebar-modal/src/Modal.js
@@ -11,7 +11,7 @@ const Modal = () => {
     >
       <div className="modal-container">
         <h3>model content</h3>
-        <button className="close-model-btn" onClick={closeModal}>
+        <button className="close-modal-btn" onClick={closeModal}>
           <FaTimes></FaTimes>
         </button>
       </div>
