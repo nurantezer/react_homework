67eaa69beb093806b1c5adff5e7cde4475afeb28
nurantezer <nurantezer68@gmail.com>
2023-04-16 15:17:40 +0300

    end

--- 30_jcoktails/src/App.js
+++ 30_jcoktails/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 // import pages
 import Home from './pages/Home'
 import About from './pages/About'
