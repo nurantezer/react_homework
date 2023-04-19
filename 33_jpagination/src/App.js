9a5da7746958379f28bd081477295c1fc34bfd3b
nurantezer <nurantezer68@gmail.com>
2023-04-19 05:57:31 +0300

    follower

--- 33_jpagination/src/App.js
+++ 33_jpagination/src/App.js
@@ -1,8 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
+
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch()
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
+
+  useEffect(() => {
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, page])
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length -1
+      }
+    })
+    return prevPage
+  }
+
+  const handlePage = (index) => {
+    setPage(index)
+  }
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        
+      </section>
+    </main>
+  )
 }
 
 export default App
