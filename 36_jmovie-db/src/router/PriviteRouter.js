4f84034d01651ac46a18e04ebc6756685f3ed0a5
nurantezer <nurantezer68@gmail.com>
2023-04-30 06:41:38 +0300

    priviterouter

---
+++ 36_jmovie-db/src/router/PriviteRouter.js
@@ -0,0 +1,8 @@
+import { Navigate, Outlet } from 'react-router-dom'
+
+const PrivateRouter = () => {
+    const currentUser = true
+    return currentUser ? <Outlet /> : <Navigate to={"/"} replace />
+}
+
+export default PrivateRouter
